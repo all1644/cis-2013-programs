3fc30177ef99c88c075ef14972b31cb2cf059945
all1644 <78062878+all1644@users.noreply.github.com>
2021-02-15 12:25:39 -0600

    Add files via upload

---
+++ age.js
@@ -0,0 +1,12 @@
+<script>
+     var floatYears, floatMonths, floatFortnights, floatWeeks, floatDays;
+     floatYears = prompt("Enter current age");
+     floatMonths = floatYears*12;
+     floatFortnights = floatYears*26.08875;
+     floatWeeks = floatYears*52;
+     floatDays = floatDays*365;
+     alert("Age in months = " + floatMonths);
+     alert("Age in fortnights = " + floatFortnights);
+     alert("Age in weeks = " + floatWeeks);
+     alert("Age in days = " + floatDays);
+   </script>
