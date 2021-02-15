ee9d3ac487c804bb9dc0328cf218a76061979182
all1644 <78062878+all1644@users.noreply.github.com>
2021-02-15 12:40:41 -0600

    Add files via upload

--- age.js
+++ age.js
@@ -4,7 +4,7 @@
      floatMonths = floatYears*12;
      floatFortnights = floatYears*26.08875;
      floatWeeks = floatYears*52;
-     floatDays = floatDays*365;
+     floatDays = floatYears*365;
      alert("Age in months = " + floatMonths);
      alert("Age in fortnights = " + floatFortnights);
      alert("Age in weeks = " + floatWeeks);
